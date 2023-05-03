8e7f131f7f87190b1f040200dcd47cee331326d7
Seakleang <nhakseakleang@gmail.com>
2023-05-03 23:31:57 +0700

    change color

--- src/components/product/product-section.tsx
+++ src/components/product/product-section.tsx
@@ -4,7 +4,7 @@ import ProductItem from "./product-item";
 export default component$(() => {
     return (
         <>
-            <h5 class="text-orange-500 underline underline-offset-[12px] mt-4">Title</h5>
+            <h5 class="text-blue-800 underline underline-offset-[12px] mt-4">Title</h5>
             <div class="mt-4 grid lg:grid-cols-5 md:grid-cols-3 grid-cols-2 gap-4">
                 <ProductItem />
                 <ProductItem />
