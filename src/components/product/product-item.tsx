2aabc2115b8a18b63121b611fb402706c5416fbd
Seakleang <nhakseakleang@gmail.com>
2023-05-14 16:11:12 +0700

    add out of stock and update product layout

--- src/components/product/product-item.tsx
+++ src/components/product/product-item.tsx
@@ -5,8 +5,15 @@ import { Image } from "@unpic/qwik";
 export default component$((props: { product: ProductModel, productPage?: boolean }) => {
 
     const url = `https://ik.imagekit.io/botracomputer/ik-seo/${(props.product.images ?? "").split(",")[0].replace(".", "/" + props.product.name.replace(" ", "-") + ".")}?tr=w-${props.productPage ? 500 : 300},h-${props.productPage ? 500 : 300},c-at_max`
-    const style = "col-span-1 flex flex-col border-[2px] border-blue-600 rounded-md bg-white overflow-hidden cursor-pointer" + (props.productPage ? "" : "hover:shadow-lg ease-in-out hover:-translate-y-1 transition-all relative")
+    const style = "col-span-1 flex flex-col border-[2px] h-full w-full border-blue-600 rounded-md bg-white overflow-hidden cursor-pointer" + (props.productPage ? "" : "hover:shadow-lg ease-in-out hover:-translate-y-1 transition-all relative")
     const productUrl = props.productPage ? "#" : `/product/${props.product.brand_name?.trim()}-${props.product.name.trim()}-${props.product.id}`.replaceAll(" ", "-").replaceAll("(", "").replaceAll(")", "").replaceAll("%", "")
+
+    const event_text = props.product.event_text ?? (props.product.in_stock ? "" : "Out of stock")
+    const event_color = props.product.event_color ?? (props.product.in_stock ? "" : "#ffff0000") // red
+
+    if (!props.product.in_stock) {
+        console.log(url);
+    }
 
     return (
         <Link prefetch={true} href={productUrl}>
@@ -23,9 +30,9 @@ export default component$((props: { product: ProductModel, productPage?: boolean
                 <div class="p-1 m-1 text-center text-base text-zinc-100 font-bold rounded-[4px] bg-blue-600">
                     <span>{props.product.brand_name ?? "Botra Computer"}</span>
                 </div>
-                {(props.product.event_text != null && props.product.event_text != "") && (
-                    <div class="absolute -top-1 -left-1 p-1 pt-2 pl-2 rounded-[4px] text-white text-sm" style={"background-color:#" + props.product.event_color?.substring(3, 9) + props.product.event_color?.substring(1, 3) + ";"}>
-                        {props.product.event_text}
+                {(event_text != null && event_text != "") && (
+                    <div class="absolute -top-1 -left-1 p-1 pt-2 pl-2 rounded-[4px] text-white text-sm" style={"background-color:#" + event_color?.substring(3, 9) + event_color?.substring(1, 3) + ";"}>
+                        {event_text}
                     </div>
                 )}
             </div>
