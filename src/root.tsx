e6437038337c3f0c6b963c20f79d631d4e092efb
Seakleang <nhakseakleang@gmail.com>
2023-05-16 22:09:11 +0700

    add analytics

--- src/root.tsx
+++ src/root.tsx
@@ -3,6 +3,8 @@ import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.
 import { RouterHead } from './components/router-head/router-head';
 
 import './global.css';
+import { QwikPartytown } from './components/partytown/partytown';
+import { inject } from '@vercel/analytics';
 
 export default component$(() => {
   /**
@@ -12,12 +14,16 @@ export default component$(() => {
    * Don't remove the `<head>` and `<body>` elements.
    */
 
+  inject();
+
   return (
     <QwikCityProvider>
       <head>
         <meta charSet="utf-8" />
         <link rel="manifest" href="/manifest.json" />
         <RouterHead />
+        <QwikPartytown forward={['dataLayer.push']} />
+        <script async type="text/partytown" src="https://www.googletagmanager.com/gtag/js?id=G-7XL1BKJ430" />
       </head>
       <body lang="en">
         <RouterOutlet />
